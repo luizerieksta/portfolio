4efdfe42d7daec8eb774dfb02e016be37fc537f8
Luize <lr1965@nyu.edu>
2019-03-16 02:11:00 -0400

    First commit

---
+++ js/scripts.js
@@ -0,0 +1,101 @@
+console.log("HOLAAAA");
+
+
+	$("#click-here-1").click(function() {
+  
+   		var label = $("#click-here-1").text().trim();
+
+		if(label == "read less") {
+	    	$("#click-here-1").text("read more");
+	    	$("#info-box-1").hide();
+
+	   	} else {
+	    	$("#click-here-1").text("read less");
+	    	$("#info-box-1").show();
+	   	}
+    
+  	});
+
+	$("#click-here-2").click(function() {
+  
+   		var label = $("#click-here-2").text().trim();
+
+		if(label == "read less") {
+	    	$("#click-here-2").text("read more");
+	    	$("#info-box-2").hide();
+
+	   	} else {
+	    	$("#click-here-2").text("read less");
+	    	$("#info-box-2").show();
+	   	}
+    
+  	});
+
+	$("#click-here-3").click(function() {
+  
+   		var label = $("#click-here-3").text().trim();
+
+		if(label == "read less") {
+	    	$("#click-here-3").text("read more");
+	    	$("#info-box-3").hide();
+
+	   	} else {
+	    	$("#click-here-3").text("read less");
+	    	$("#info-box-3").show();
+	   	}
+    
+  	});
+
+
+	$("#click-here-4").click(function() {
+  
+   		var label = $("#click-here-4").text().trim();
+
+		if(label == "read less") {
+	    	$("#click-here-4").text("read more");
+	    	$("#info-box-4").hide();
+
+	   	} else {
+	    	$("#click-here-4").text("read less");
+	    	$("#info-box-4").show();
+	   	}
+    
+  	});
+
+
+	$("#click-here-5").click(function() {
+  
+   		var label = $("#click-here-5").text().trim();
+
+		if(label == "read less") {
+	    	$("#click-here-5").text("read more");
+	    	$("#info-box-5").hide();
+
+	   	} else {
+	    	$("#click-here-5").text("read less");
+	    	$("#info-box-5").show();
+	   	}
+    
+  	});
+
+
+ 	$("#click-here-6").click(function() {
+  
+   		var label = $("#click-here-6").text().trim();
+
+		if(label == "read less") {
+	    	$("#click-here-6").text("read more");
+	    	$("#info-box-6").hide();
+
+	   	} else {
+	    	$("#click-here-6").text("read less");
+	    	$("#info-box-6").show();
+	   	}
+    
+  	});
+
+
+
+
+
+
